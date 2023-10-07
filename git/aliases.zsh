d9bb68b2aebecce0a73d7d165acf2fb0b2471dbd
Ethan Dobrowolski <ethan.dobrowolski@gmail.com>
2023-10-06 21:30:08 -0400

    Update git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -3,18 +3,22 @@
 # log
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gs='git status'
+alias gcon='git config'
 
 # local
 alias gi='git init'
 alias ga='git add'
-alias gaa='git add -a'
-alias gc='git commit'
-alias gcm='git commit -m'
-alias gca='git commit -a'
-alias gco='git checkout'
+alias gcom='git commit'
+alias gch='git checkout'
 alias gb='git branch'
+alias gd='git diff'
+alias greb='git rebase -i'
+alias grebb='git rebase'
+alias gres='git restore'
+alias grst='git reset'
 
 # remote
 alias gra='git remote add'
-alias gp='git push'
-alias gl='git pull'
+alias gpush='git push'
+alias gpull='git pull'
+
